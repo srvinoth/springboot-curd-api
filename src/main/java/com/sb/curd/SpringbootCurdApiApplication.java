bc1ace6c87f2ac3a31be15c5f7e3b151f1a47fb0
S.Vinoth Kumar <srvinoth@gmail.com>
2018-04-21 18:35:47 +0400

    Update SpringbootCurdApiApplication.java

--- src/main/java/com/sb/curd/SpringbootCurdApiApplication.java
+++ src/main/java/com/sb/curd/SpringbootCurdApiApplication.java
@@ -2,8 +2,10 @@ package com.sb.curd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringbootCurdApiApplication {
 
 	public static void main(String[] args) {
